87df38c291aaac20fec91bf99fc7c2043eda7c64
Filipewp <filipepatricio@hotmail.com.br>
2020-04-21 17:10:55 -0300

    Multijogador

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody rb;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+
+    }
+
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+
+       
+    }
+     void LateUpdate()
+    {
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLenght;
+
+        if (groundPlane.Raycast(cameraRay, out rayLenght))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLenght);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
+
+       
+    }
+
+
+}
