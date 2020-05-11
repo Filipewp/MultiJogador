e75e06ea622293166fac95223889041d5fa25223
Filipewp <filipepatricio@hotmail.com.br>
2020-05-11 14:54:25 -0300

    update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour
+using UnityEngine.Networking;
+public class PlayerMovement : NetworkBehaviour
 {
     public float moveSpeed;
     private Rigidbody rb;
@@ -21,22 +21,27 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput * moveSpeed;
-
+        if (isLocalPlayer)
+        {
+            moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            moveVelocity = moveInput * moveSpeed;
+        }
        
     }
      void LateUpdate()
     {
-        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        float rayLenght;
-
-        if (groundPlane.Raycast(cameraRay, out rayLenght))
+        if (isLocalPlayer)
         {
-            Vector3 pointToLook = cameraRay.GetPoint(rayLenght);
+            Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+            Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+            float rayLenght;
 
-            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+            if (groundPlane.Raycast(cameraRay, out rayLenght))
+            {
+                Vector3 pointToLook = cameraRay.GetPoint(rayLenght);
+
+                transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+            }
         }
     }
     void FixedUpdate()
